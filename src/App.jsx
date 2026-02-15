ac19580b94654c0b3d1938231b62670c6c61ae00
Bry Nguyen <brynguyen1331@gmail.com>
2026-02-15 15:28:26 -0800

    style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
+import { HomePage } from './pages/HomePage'
+
 function App() {
-  return (
-    <div style={{ padding: '2rem', color: 'var(--text-primary)' }}>
-      <h1>NUTS.GG Components</h1>
-      <p>Run <code>npm run storybook</code> to view the component library.</p>
-    </div>
-  )
+  return <HomePage />
 }
 
 export default App
